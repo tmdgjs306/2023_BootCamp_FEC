56aeb44f3485c8db3b2090b0bf0ae47e8cd9f155
tmdgjs306 <58868717+tmdgjs306@users.noreply.github.com>
2023-07-24 10:35:01 +0900

    Spring_Init

---
+++ Temp_Spring/src/main/java/com/example/temp_spring/TempSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.temp_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TempSpringApplication.class, args);
+    }
+
+}
