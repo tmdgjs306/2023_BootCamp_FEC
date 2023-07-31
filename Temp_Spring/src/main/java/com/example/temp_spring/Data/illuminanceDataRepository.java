10095c143c8010e6679c89cb3bfd19a2611e3d2c
jisoo123456 <imearth0913@gmail.com>
2023-07-31 18:51:50 +0900

    db_test_v0.0

--- Temp_Spring/src/main/java/com/example/temp_spring/Data/illuminanceDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.temp_spring.Data;
-
-import java.util.List;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface illuminanceDataRepository extends MongoRepository<illuminanceData, String> {
-
-    public illuminanceData findByValue(double value);
-}
