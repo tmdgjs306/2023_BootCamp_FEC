35dfdbc2339de393d32002e34d610a00428ac117
jisoo123456 <imearth0913@gmail.com>
2023-08-01 14:34:17 +0900

    db_test_v0.2

--- Temp_Spring/src/main/java/com/example/temp_spring/TempSpringApplication.java
+++ Temp_Spring/src/main/java/com/example/temp_spring/TempSpringApplication.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import java.util.Arrays;
 import java.util.List;
 
-@SpringBootApplication(exlude = [DataSourceAutoConfiguration::class])
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class TempSpringApplication implements CommandLineRunner {
 
     @Autowired
